f4b4089b5db46bb3bc1f89b2b55441c062495b7f
Brady Edwards <brady.edwards16@gmail.com>
2025-01-18 20:47:46 -0500

    Docker optimizations

--- src/configuration.rs
+++ src/configuration.rs
@@ -21,6 +21,19 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> Secret<String> {
+        Secret::new(format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+            self.database_name
+        ))
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
@@ -71,18 +84,5 @@ impl TryFrom<String> for Environment {
                 other
             )),
         }
-    }
-}
-
-impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        ))
     }
 }
